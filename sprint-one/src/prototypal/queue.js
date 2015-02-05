de0f6b96deb2b12fc1560ae6d565c85705db1f6b
pravipati <psravipati@gmail.com>
2015-02-05 10:40:41 -0800

    Complete prototypal pattern queue function and pass tests.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.front = 0;
+  queue.length = 0;
+  queue.storage = {};
+  return queue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function(){
+  if (this.front > this.length){
+    return 0;
+  }
+  return this.length - this.front;
+}
+
+queueMethods.enqueue = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+}
+
+queueMethods.dequeue = function(){
+  var result = this.storage[this.front];
+  this.front++;
+  return result;
+}
 
 
