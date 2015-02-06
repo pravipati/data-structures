00f2e3e11432c4f460fc16f61cec229e5168461b
pravipati <psravipati@gmail.com>
2015-02-05 16:31:52 -0800

    Currently passing 6/7 tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,24 +2,58 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var key = 0;
 
   list.addToTail = function(value){
+    // store new node in variable
+    var node = Node(value);
+    //if list.tail == null
+    if (list.tail === null){
+    //assign list.tail to Node
+      list.tail = node;
+      list.head = node;
+      list[key] = node;
+      key++;
+    }
+    //if list.tail already has a node, change list.tail's next value to node
+    list.tail.next = node;
+    list.tail = node;
+    list[key] = node;
+    key++;
   };
 
   list.removeHead = function(){
+    if (list.head){
+      var result = list.head;
+      list.head = list.head.next;
+      console.log(result.key);
+      delete list[result.key]
+      return result.value;
+    }
+
   };
 
   list.contains = function(target){
+    for (var key in list) {
+      if (list[key].value === target){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
 };
 
+
+
 var Node = function(value){
   var node = {};
-
+  var key = this.key
+  node.key = key;
   node.value = value;
   node.next = null;
+
 
   return node;
 };
